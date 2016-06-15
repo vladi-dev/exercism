67094b964e3120781f5c3fc99eacac408e73b510
Vladi <emve.dev@yandex.ru>
2016-06-14 21:11:47 -0700

    Use collections.Counter for words count

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
+import collections
 
 
 def word_count(str):
     r = dict()
     str = str.decode('utf-8')
     str = re.sub('[\W\,\_]', ' ', str.lower(), flags=re.UNICODE).strip()
-    for w in str.split():
-        r[w] = r[w] + 1 if w in r else 1
-    return r
+    return collections.Counter(str.split())
